f131557a7710b050c4a4636f08f426ffc5ffd3e7
Matteo  Villa <matteovilla@matteos-air-2.home>
2025-04-22 11:31:14 +0200

    first upload

---
+++ src/PolygonalMesh.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+#include "Eigen/Eigen"
+
+using namespace std;
+using namespace Eigen;
+
+
+namespace PolygonalLibrary {
+
+struct PolygonalMesh
+{
+    unsigned int NumCell0Ds;
+    unsigned int NumCell1Ds;
+    unsigned int NumCell2Ds;
+
+    vector<unsigned int> Cell0DsId;
+    vector<unsigned int> Cell1DsId;
+    vector<unsigned int> Cell2DsId;
+
+    Eigen::MatrixXd Cell0DsCoordinates;
+    Eigen::MatrixXi Cell1DsExtrema;
+    vector<unsigned int*> Cell2DsVertices;
+    vector<unsigned int*> Cell2DsEdges;
+
+    std::map<unsigned int, list<unsigned int>> MarkerCell0Ds = {};
+    std::map<unsigned int, list<unsigned int>> MarkerCell1Ds = {};
+};
+
+}
+
